e59e4da09f64db915a0c24926127064d512de05c
ziadshebl <ziadshebl@gmail.com>
2021-05-23 12:38:43 +0200

    First Commit

---
+++ src/main/java/com/webbasedcrawlerapt/WebBasedCrawlerProject/WebBasedCrawlerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webbasedcrawlerapt.WebBasedCrawlerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedCrawlerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedCrawlerProjectApplication.class, args);
+	}
+
+}
