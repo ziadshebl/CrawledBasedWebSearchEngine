ff4aa0aa381e05b7fb9bcee4f3b812477cce1adb
ziadshebl <ziadshebl@gmail.com>
2021-05-31 17:43:18 +0200

    same

--- src/main/java/com/webbasedcrawlerapt/WebBasedCrawlerProject/Endpoints/WordController.java
+++ src/main/java/com/webbasedcrawlerapt/WebBasedCrawlerProject/Endpoints/WordController.java
@@ -3,6 +3,7 @@ package com.webbasedcrawlerapt.WebBasedCrawlerProject.Endpoints;
 import com.webbasedcrawlerapt.WebBasedCrawlerProject.Services.WordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class WordController {
     }
 
     
-    @RequestMapping(path="/{word}")
+    @GetMapping(params = "/{word}")
     public ResponseEntity<?> getAllReservations(@PathVariable("word") String word) {
 
         //word = word.toLowerCase();
