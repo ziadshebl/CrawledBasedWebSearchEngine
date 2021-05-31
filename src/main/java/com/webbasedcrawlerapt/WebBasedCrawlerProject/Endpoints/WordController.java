18fa0f00fe38c34c0928cdd8f541021af3003dfa
ziadshebl <ziadshebl@gmail.com>
2021-05-31 19:06:10 +0200

    Same

--- src/main/java/com/webbasedcrawlerapt/WebBasedCrawlerProject/Endpoints/WordController.java
+++ src/main/java/com/webbasedcrawlerapt/WebBasedCrawlerProject/Endpoints/WordController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import opennlp.tools.stemmer.PorterStemmer;
 
@@ -20,22 +21,24 @@ public class WordController {
     }
 
     
-    @RequestMapping(params = "/{word}")
+    // @RequestMapping(params = "/{word}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{word}")
     public ResponseEntity<?> getAllReservations(@PathVariable("word") String word) {
 
         //word = word.toLowerCase();
+        System.out.println("INSIDEEEE");
         String stemmedWord = Stemmer.stem(word);
         return wordService.getAllWebsitesByWord(stemmedWord);       
 
     }
 
-    @RequestMapping(params = "/")
-    public String root(@PathVariable("word") String word) {
+    // @RequestMapping(params = "/")
+    // public String root(@PathVariable("word") String word) {
 
       
-        return "Welcome to doodle";       
+    //     return "Welcome to doodle";       
 
-    }
+    // }
 
 }
 
